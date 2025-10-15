f5b6e2267f24e2414343611bca8e808b8b8a94d9
Gibekkk <gfoucauld@student.ciputra.ac.id>
2025-10-15 18:11:34 +0800

    p

--- offline.js
+++ offline.js
@@ -1,13 +1,14 @@
 ﻿{
-	"version": 1760515146,
+	"version": 1760522992,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/playerplaceholder-sheet0.png",
-		"images/enemyplaceholder-sheet0.png",
+		"images/bossplaceholder-sheet0.png",
 		"images/enemyprojectileplaceholder-sheet0.png",
+		"images/enemyspawner-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
