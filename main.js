66680518e85c55d60e0ee8cebedb3a98463b1b90
Oreo-1 <121080759+Oreo-1@users.noreply.github.com>
2025-10-17 14:40:30 +0800

    electron fix
    ts finally worked

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require('electron/main')
 
-function createWindow() {
+const createWindow = () => {
   const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js'),
-    // },
-  });
+    width: 800,
+    height: 600
+  })
 
-  win.loadFile(path.join(__dirname, 'index.html'));
-  win.removeMenu(); // optional: remove the menu bar
+  win.loadFile('index.html');
+  win.removeMenu(); // comment to display menu window
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
