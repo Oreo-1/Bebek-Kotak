c7379b4acd31559443ed592fad1231557476bccf
Gilbert Winardy <www.gilbertwinardy@gmail.com>
2025-11-21 09:10:42 +0800

    bug fix

--- c2runtime.js
+++ c2runtime.js
@@ -25049,6 +25049,117 @@ cr.behaviors.Sin = function(runtime)
 }());
 ;
 ;
+cr.behaviors.scrollto = function(runtime)
+{
+	this.runtime = runtime;
+	this.shakeMag = 0;
+	this.shakeStart = 0;
+	this.shakeEnd = 0;
+	this.shakeMode = 0;
+};
+(function ()
+{
+	var behaviorProto = cr.behaviors.scrollto.prototype;
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	var behtypeProto = behaviorProto.Type.prototype;
+	behtypeProto.onCreate = function()
+	{
+	};
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	var behinstProto = behaviorProto.Instance.prototype;
+	behinstProto.onCreate = function()
+	{
+		this.enabled = (this.properties[0] !== 0);
+	};
+	behinstProto.saveToJSON = function ()
+	{
+		return {
+			"smg": this.behavior.shakeMag,
+			"ss": this.behavior.shakeStart,
+			"se": this.behavior.shakeEnd,
+			"smd": this.behavior.shakeMode
+		};
+	};
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.behavior.shakeMag = o["smg"];
+		this.behavior.shakeStart = o["ss"];
+		this.behavior.shakeEnd = o["se"];
+		this.behavior.shakeMode = o["smd"];
+	};
+	behinstProto.tick = function ()
+	{
+	};
+	function getScrollToBehavior(inst)
+	{
+		var i, len, binst;
+		for (i = 0, len = inst.behavior_insts.length; i < len; ++i)
+		{
+			binst = inst.behavior_insts[i];
+			if (binst.behavior instanceof cr.behaviors.scrollto)
+				return binst;
+		}
+		return null;
+	};
+	behinstProto.tick2 = function ()
+	{
+		if (!this.enabled)
+			return;
+		var all = this.behavior.my_instances.valuesRef();
+		var sumx = 0, sumy = 0;
+		var i, len, binst, count = 0;
+		for (i = 0, len = all.length; i < len; i++)
+		{
+			binst = getScrollToBehavior(all[i]);
+			if (!binst || !binst.enabled)
+				continue;
+			sumx += all[i].x;
+			sumy += all[i].y;
+			++count;
+		}
+		var layout = this.inst.layer.layout;
+		var now = this.runtime.kahanTime.sum;
+		var offx = 0, offy = 0;
+		if (now >= this.behavior.shakeStart && now < this.behavior.shakeEnd)
+		{
+			var mag = this.behavior.shakeMag * Math.min(this.runtime.timescale, 1);
+			if (this.behavior.shakeMode === 0)
+				mag *= 1 - (now - this.behavior.shakeStart) / (this.behavior.shakeEnd - this.behavior.shakeStart);
+			var a = Math.random() * Math.PI * 2;
+			var d = Math.random() * mag;
+			offx = Math.cos(a) * d;
+			offy = Math.sin(a) * d;
+		}
+		layout.scrollToX(sumx / count + offx);
+		layout.scrollToY(sumy / count + offy);
+	};
+	function Acts() {};
+	Acts.prototype.Shake = function (mag, dur, mode)
+	{
+		this.behavior.shakeMag = mag;
+		this.behavior.shakeStart = this.runtime.kahanTime.sum;
+		this.behavior.shakeEnd = this.behavior.shakeStart + dur;
+		this.behavior.shakeMode = mode;
+	};
+	Acts.prototype.SetEnabled = function (e)
+	{
+		this.enabled = (e !== 0);
+	};
+	behaviorProto.acts = new Acts();
+}());
+;
+;
 cr.behaviors.shadowcaster = function(runtime)
 {
 	this.runtime = runtime;
@@ -25131,7 +25242,6 @@ cr.behaviors.shadowcaster = function(runtime)
 	behaviorProto.exps = new Exps();
 }());
 cr.getObjectRefTable = function () { return [
-	cr.plugins_.Dictionary,
 	cr.plugins_.Function,
 	cr.plugins_.Keyboard,
 	cr.plugins_.Mouse,
@@ -25143,8 +25253,10 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Touch,
 	cr.plugins_.AJAX,
 	cr.plugins_.Audio,
+	cr.plugins_.Dictionary,
 	cr.plugins_.Browser,
 	cr.behaviors.Rotate,
+	cr.behaviors.scrollto,
 	cr.behaviors.Sin,
 	cr.behaviors.shadowcaster,
 	cr.behaviors.Bullet,
@@ -25166,12 +25278,12 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Function.prototype.acts.CallFunction,
 	cr.plugins_.Text.prototype.exps.UID,
 	cr.system_object.prototype.cnds.CompareVar,
+	cr.plugins_.Sprite.prototype.acts.SetVisible,
 	cr.plugins_.Keyboard.prototype.cnds.OnAnyKey,
 	cr.plugins_.Text.prototype.cnds.IsOnScreen,
 	cr.plugins_.progressbar.prototype.cnds.CompareProgress,
 	cr.plugins_.Keyboard.prototype.exps.StringFromKeyCode,
 	cr.plugins_.Keyboard.prototype.exps.LastKeyCode,
-	cr.system_object.prototype.exps.trim,
 	cr.plugins_.Keyboard.prototype.cnds.IsKeyDown,
 	cr.system_object.prototype.cnds.Else,
 	cr.system_object.prototype.exps.lowercase,
@@ -25246,6 +25358,7 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Text.prototype.acts.SetWidth,
 	cr.plugins_.Sprite.prototype.cnds.PickByUID,
 	cr.plugins_.Sprite.prototype.acts.AddInstanceVar,
+	cr.system_object.prototype.exps.trim,
 	cr.plugins_.LocalStorage.prototype.acts.CheckItemExists,
 	cr.plugins_.progressbar.prototype.exps.Progress,
 	cr.system_object.prototype.cnds.ForEachOrdered,
@@ -25272,7 +25385,6 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Sprite.prototype.cnds.CompareFrame,
 	cr.system_object.prototype.exps.layoutname,
 	cr.system_object.prototype.acts.SetObjectTimescale,
-	cr.plugins_.Sprite.prototype.acts.SetVisible,
 	cr.system_object.prototype.acts.RestoreObjectTimescale,
 	cr.plugins_.Sprite.prototype.cnds.CompareX,
 	cr.plugins_.Sprite.prototype.acts.SetPosToObject,
@@ -25296,6 +25408,9 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Text.prototype.cnds.OnDestroyed,
 	cr.plugins_.Text.prototype.exps.Width,
 	cr.plugins_.Text.prototype.exps.Height,
+	cr.plugins_.Sprite.prototype.acts.MoveToTop,
+	cr.plugins_.Text.prototype.acts.MoveToTop,
+	cr.plugins_.Sprite.prototype.cnds.CompareY,
 	cr.plugins_.Text.prototype.acts.SetPos,
 	cr.plugins_.Mouse.prototype.cnds.OnRelease,
 	cr.plugins_.Mouse.prototype.cnds.IsOverObject,
@@ -25310,6 +25425,5 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Audio.prototype.cnds.IsTagPlaying,
 	cr.plugins_.Audio.prototype.acts.SetMuted,
 	cr.system_object.prototype.exps.choose,
-	cr.behaviors.Sin.prototype.acts.SetMagnitude,
-	cr.plugins_.Sprite.prototype.cnds.CompareY
+	cr.behaviors.Sin.prototype.acts.SetMagnitude
 ];};
